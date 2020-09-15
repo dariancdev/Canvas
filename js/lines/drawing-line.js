fb8f873108844e7a06fdc49b9b2f4b9acf8e72fe
arran101 <arran101@gmail.com>
2020-09-15 20:37:03 +0800

    added in functionalities, buttons not working

---
+++ js/lines/drawing-line.js
@@ -0,0 +1,50 @@
+class DrawingLine extends PaintFunction {
+    // This class extends the PaintFunction class
+    constructor(contextReal) {
+      super();
+      this.context = contextReal;
+    }
+  
+    onMouseDown(coord, event) {
+      this.context.strokeStyle = strokeColour.value;
+      this.context.lineJoin = "round";
+      this.context.lineCap = "round";
+      this.context.lineWidth = lineWidth.value;
+      this.context.beginPath();
+      this.context.moveTo(coord[0], coord[1]);
+      this.draw(coord[0], coord[1]);
+    }
+    onDragging(coord, event) {
+      this.draw(coord[0], coord[1]);
+    }
+  
+    onMouseMove() {}
+    onMouseUp() {
+      this.onFinish();
+    }
+    onMouseLeave() {}
+    onMouseEnter() {}
+  
+    draw(x, y) {
+      this.context.lineTo(x, y);
+      this.context.moveTo(x, y);
+      this.context.closePath();
+      this.context.stroke();
+    }
+  // this onFinish code works as well
+    // onFinish(){
+    //   pushedArray[step] = new Image();
+    //   pushedArray[step].src = canvasReal.toDataURL();
+    //   step++;
+    //   console.log(step)
+    // }
+    onFinish(){
+      step++;
+      console.log(step);
+      if(step < pushedArray.length){
+        pushedArray.length = step;
+      }
+      pushedArray.push(canvasReal.toDataURL());
+    }
+  }
+  
