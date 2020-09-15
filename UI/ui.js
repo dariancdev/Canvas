6982a335879c714a0c36b62c0b932dbb4a66650c
Darian <apple@apples-mbp.mynet>
2020-09-15 14:35:00 +0800

    second push for UI

--- UI/ui.js
+++ UI/ui.js
@@ -1,3 +1,5 @@
+/* ------------------ SHOW & HIDE DROP-DOWN MENU ------------------ */
+
 var a;
 function showHideStroke() {
     if (a == 1) {
@@ -5,6 +7,8 @@ function showHideStroke() {
         return a = 0;
     } else {
         document.getElementById("stroke-items").style.display = "block";
+        document.getElementById("shape-items").style.display = "none";
+        document.getElementById("tool-items").style.display = "none";
         return a = 1;
     }
 }
@@ -15,6 +19,8 @@ function showHideShape() {
         return a = 0;
     } else {
         document.getElementById("shape-items").style.display = "block";
+        document.getElementById("stroke-items").style.display = "none";
+        document.getElementById("tool-items").style.display = "none";
         return a = 1;
     }
 }
@@ -25,6 +31,8 @@ function showHideTools() {
         return a = 0;
     } else {
         document.getElementById("tool-items").style.display = "block";
+        document.getElementById("stroke-items").style.display = "none";
+        document.getElementById("shape-items").style.display = "none";
         return a = 1;
     }
 }
