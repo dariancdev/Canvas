a07f06d15272fd65908c5ca7983f2da66b91aeb0
arran101 <arran101@gmail.com>
2020-09-16 22:25:31 +0800

    cleaned up code

--- js/shapes/drawing-rectangle.js
+++ js/shapes/drawing-rectangle.js
@@ -1,40 +1,46 @@
-    onMouseDown(coord, event) {
-      this.contextReal.fillStyle = objectFill.value;
-      this.origX = coord[0];
-      this.origY = coord[1];
-    }
-    onDragging(coord, event) {
-      this.contextDraft.fillStyle = objectFill.value;
-      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-      this.contextDraft.fillRect(
-        this.origX,
-        this.origY,
-        coord[0] - this.origX,
-        coord[1] - this.origY
-      );
-    }
-  
-    onMouseMove() {}
-    onMouseUp(coord) {
-      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-      this.contextReal.fillRect(
-        this.origX,
-        this.origY,
-        coord[0] - this.origX,
-        coord[1] - this.origY
-      );
-      this.onFinish();
-    }
-    onMouseLeave() {}
-    onMouseEnter() {}
-  
-    onFinish(){
-      step++;
-      console.log(step);
-      if(step < pushedArray.length){
-        pushedArray.length = step;
-      }
-      pushedArray.push(canvasReal.toDataURL());
-    }
+class DrawingRectangle extends PaintFunction {
+  constructor(contextReal, contextDraft) {
+    super();
+    this.contextReal = contextReal;
+    this.contextDraft = contextDraft;
   }
 
+  onMouseDown(coord, event) {
+    this.contextReal.fillStyle = objectFill.value;
+    this.origX = coord[0];
+    this.origY = coord[1];
+  }
+  onDragging(coord, event) {
+    this.contextDraft.fillStyle = objectFill.value;
+    this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+    this.contextDraft.fillRect(
+      this.origX,
+      this.origY,
+      coord[0] - this.origX,
+      coord[1] - this.origY
+    );
+  }
+
+  onMouseMove() {}
+  onMouseUp(coord) {
+    this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+    this.contextReal.fillRect(
+      this.origX,
+      this.origY,
+      coord[0] - this.origX,
+      coord[1] - this.origY
+    );
+    this.onFinish();
+  }
+  onMouseLeave() {}
+  onMouseEnter() {}
+  
+  onFinish(){
+    step++;
+    console.log(step);
+    if(step < pushedArray.length){
+      pushedArray.length = step;
+    }
+    pushedArray.push(canvasReal.toDataURL());
+  }
+}
