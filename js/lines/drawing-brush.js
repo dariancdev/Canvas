93826e2974e2af8e8385b6b0a4cc78b4aac208ba
arran101 <arran101@gmail.com>
2020-09-17 00:19:40 +0800

    issues with rectangle and line

--- js/lines/drawing-brush.js
+++ js/lines/drawing-brush.js
@@ -1,55 +1,46 @@
 class DrawingBrush extends PaintFunction {
-    // This class extends the PaintFunction class
-    constructor(contextReal) {
-      super();
-      this.contextReal = contextReal;
-      this.contextDraft = contextDraft; 
-    }
-  
-    onMouseDown(coord, event) {
-      this.context.strokeStyle = strokeColour.value;
-      this.context.lineJoin = "round";
-      this.context.lineCap = "round";
-      this.context.lineWidth = lineWidth.value;
-      this.context.shadowBlur = 10;
-      this.context.shadowColor = strokeColour.value;
-      this.context.beginPath();
-      this.context.moveTo(coord[0], coord[1]);
-      this.draw(coord[0], coord[1]);
-    }
-    onDragging(coord, event) {
-      this.draw(coord[0], coord[1]);
-    }
-  
-    onMouseMove() {}
-    onMouseUp() {
-      this.onFinish();
-      this.contextDraft.shadowBlur = 0;
-      this.contextDraft.shadowColor = '';
-    }
-    onMouseLeave() {}
-    onMouseEnter() {}
-  
-    draw(x, y) {
-      this.context.lineTo(x, y);
-      this.context.moveTo(x, y);
-      this.context.closePath();
-      this.context.stroke();
-    }
-  // this onFinish code works as well
-    // onFinish(){
-    //   pushedArray[step] = new Image();
-    //   pushedArray[step].src = canvasReal.toDataURL();
-    //   step++;
-    //   console.log(step)
-    // }
-    onFinish(){
-      step++;
-      console.log(step);
-      if(step < pushedArray.length){
-        pushedArray.length = step;
-      }
-      pushedArray.push(canvasReal.toDataURL());
-    }
+  constructor(contextReal) {
+    super();
+    this.context = contextReal;
   }
+
+  onMouseDown(coord, event) {
+    this.context.strokeStyle = strokeColour.value;
+    this.context.shadowColor = strokeColour.value;
+    this.context.lineJoin = "round";
+    this.context.lineCap = "round";
+    this.context.shadowBlur = 10;
+    this.context.lineWidth = lineWidth.value;
+    this.context.beginPath();
+    this.context.moveTo(coord[0], coord[1]);
+    this.draw(coord[0], coord[1]);
+  }
+  onDragging(coord, event) {
+    this.draw(coord[0], coord[1]);
+  }
+
+  onMouseMove() {}
+  onMouseUp() {
+    this.onFinish();
+  }
+  onMouseLeave() {}
+  onMouseEnter() {}
+
+  draw(x, y) {
+    this.context.lineTo(x, y);
+    this.context.moveTo(x, y);
+    this.context.closePath();
+    this.context.stroke();
+  }
+
+  onFinish(){
+    step++;
+    console.log(step);
+    if(step < pushedArray.length){
+      pushedArray.length = step;
+    }
+    pushedArray.push(canvasReal.toDataURL());
+  }
+}
+
   
--- js/shapes/drawing-rectangle.js
+++ js/shapes/drawing-rectangle.js
@@ -7,29 +7,53 @@ class DrawingRectangle extends PaintFunction {
 
   onMouseDown(coord, event) {
     this.contextReal.fillStyle = objectFill.value;
+    this.contextReal.lineWidth = lineWidth.value;
+    this.contextReal.strokeStyle = strokeColour.value;
     this.origX = coord[0];
     this.origY = coord[1];
   }
   onDragging(coord, event) {
     this.contextDraft.fillStyle = objectFill.value;
+    this.contextDraft.lineWidth = lineWidth.value;
+    this.contextDraft.strokeStyle = strokeColour.value;
     this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+    this.contextDraft.beginPath();
+    this.contextDraft.strokeRect(
+      this.origX,
+      this.origY,
+      coord[0] - this.origX,
+      coord[1] - this.origY
+    );
     this.contextDraft.fillRect(
       this.origX,
       this.origY,
       coord[0] - this.origX,
       coord[1] - this.origY
     );
+    this.contextDraft.fill();
+    this.contextDraft.stroke();
+    this.contextDraft.closePath();
   }
 
   onMouseMove() {}
   onMouseUp(coord) {
     this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+    this.contextReal.beginPath();
     this.contextReal.fillRect(
       this.origX,
       this.origY,
       coord[0] - this.origX,
       coord[1] - this.origY
     );
+    this.contextDraft.strokeRect(
+      this.origX,
+      this.origY,
+      coord[0] - this.origX,
+      coord[1] - this.origY
+    );
+    this.contextReal.fill();
+    this.contextReal.stroke();
+    this.contextReal.closePath();
     this.onFinish();
   }
   onMouseLeave() {}
