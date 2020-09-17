3aa9cc6ae8cb45f7b4f81321144a02c19f0118e4
arran101 <arran101@gmail.com>
2020-09-17 19:47:57 +0800

    quadratic curve working

--- js/lines/quadratic-curve.js
+++ js/lines/quadratic-curve.js
@@ -3,36 +3,61 @@ class QuadraticCurve extends PaintFunction{
         super();
         this.contextReal = contextReal;
         this.contextDraft = contextDraft;
+        this.actionCounter = 0;
       }
       onMouseDown(coord) {
-        this.contextReal.strokeStyle = strokeColour.value;
-        this.contextReal.lineJoin = "round";
-        this.contextReal.lineCap = "round";
-        this.contextReal.lineWidth = lineWidth.value;
-        this.origX = coord[0];
-        this.origY = coord[1];
-        this.contextReal.beginPath();
-        this.contextReal.moveTo(this.origX, this.origY);
+        //   console.log(this.actionCounter)
       }
-      onDragging(coord) {
+      onDragging(coord) {}
+    
+      onMouseMove(coord) {
         this.contextDraft.strokeStyle = strokeColour.value;
         this.contextDraft.lineJoin = "round";
         this.contextDraft.lineCap = "round";
         this.contextDraft.lineWidth = lineWidth.value;
-        this.contextDraft.closePath();
-        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-        this.contextDraft.beginPath();
-        this.contextDraft.moveTo(this.origX, this.origY);
-        this.contextDraft.lineTo(coord[0],coord[1]);
-        this.contextDraft.stroke();
+
+        if(this.actionCounter === 1) {
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.drawStraight(this.origX,this.origY,coord[0],coord[1],this.contextDraft);
+
+        } else if(this.actionCounter === 2){
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.drawQuad(coord[0],coord[1],this.contextDraft);
+            this.drawDotted(this.origX,this.origY,coord[0],coord[1],this.contextDraft);
+            this.drawDotted(this.finalX,this.finalY,coord[0], coord[1], this.contextDraft);
+            this.drawTopDot(coord[0],coord[1],this.contextDraft);
+        }
       }
-    
-      onMouseMove() {}
       onMouseUp(coord) {
-        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-        this.contextReal.lineTo(coord[0],coord[1]);
-        this.contextReal.stroke();
-        this.onFinish();
+        this.contextReal.strokeStyle = strokeColour.value;
+        this.contextReal.lineJoin = "round";
+        this.contextReal.lineCap = "round";
+        this.contextReal.lineWidth = lineWidth.value;
+
+        if(this.actionCounter === 0){
+            this.origX = coord[0];
+            this.origY = coord[1];
+
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.actionCounter++
+            console.log(this.actionCounter)
+
+        } else if(this.actionCounter === 1) {
+            this.finalX = coord[0];
+            this.finalY = coord[1];
+
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.drawQuad(coord[0],coord[1],this.contextDraft);
+            this.actionCounter++
+            console.log(this.actionCounter)
+
+        } else if(this.actionCounter === 2){
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.drawQuad(coord[0],coord[1],this.contextReal);
+            this.actionCounter = 0;
+            
+            this.onFinish();
+        }
         this.contextReal.lineCap = "butt";
         this.contextReal.lineJoin = "miter";
       }
@@ -47,6 +72,38 @@ class QuadraticCurve extends PaintFunction{
         }
         pushedArray.push(canvasReal.toDataURL());
       }
-    
-    
+    //to make the first straight line 
+    drawStraight(Xstart, Ystart, Xend, Yend, context) {
+        context.beginPath();
+        context.moveTo(Xstart, Ystart);
+        context.lineTo(Xend, Yend);
+        context.stroke();
+    }
+    //to make the curve
+    drawQuad(Xstart, Ystart, context) {
+        context.beginPath();
+        context.moveTo(this.origX, this.origY);
+        context.quadraticCurveTo(Xstart, Ystart, this.finalX, this.finalY)
+        context.stroke();
+    }
+    //to make the red dotted line
+    drawDotted(X, Y, Xstart, Ystart, context) {
+        context.save()
+        context.strokeStyle = "red"
+        context.lineWidth = 2;
+        context.setLineDash([3, 3]);
+        context.beginPath();
+        context.moveTo(X, Y);
+        context.lineTo(Xstart, Ystart);
+        context.stroke();
+        context.restore()
+    }
+    //to make the small red dot at the top
+    drawTopDot(Xstart, Ystart, context) {
+        context.save()
+        context.fillStyle = "red"
+        context.fillRect(Xstart - 5, Ystart - 5, 8, 8)
+        context.restore()
+    }
 }
+
--- js/utilities/unused-functions.js
+++ js/utilities/unused-functions.js
@@ -41,4 +41,67 @@
 
 // there were two seperate undo/redo/onFinish functions done, because before I thought i'd made a mistake with how i'd done the functions. I didn't, the issue was something else, both actually work
 
- 
+ /******************************I wanted to do the quadratic curve the way I saw on Thomas Corbin's canvas cos i thought it worked way better. I can't remember but i don't think this one is working anyways, i was missing some things
+  ****************************/
+// class QuadraticCurve extends PaintFunction{
+    //     constructor(contextReal,contextDraft){
+    //         super();
+    //         this.contextReal = contextReal;
+    //         this.contextDraft = contextDraft;
+    //         this.actionCounter = 0;
+    //     }
+    
+    //         onMouseDown(coord,event){
+    //             if (this.actionCounter === 0){
+    //                 this.contextReal.lineCap = "round"; 
+    //                 this.contextDraft.lineCap = "round"; 
+    //                 this.contextReal.strokeStyle = strokeColour.value;
+    //                 this.contextDraft.strokeStyle = strokeColour.value;
+    //                 this.contextReal.lineWidth = lineWidth.value;
+    //                 this.contextDraft.lineWidth = lineWidth.value;
+    //                 this.origX = coord[0];
+    //                 this.origY = coord[1];
+    //                 this.contextReal.beginPath();
+    //                 this.contextReal.moveTo(this.origX,this.origY);
+    //             } 
+    //             else if (this.actionCounter === 1){
+    //             }
+    //         }
+    //         onDragging(coord,event){
+    //             if (this.actionCounter === 0){
+    //                 this.endX = coord[0];
+    //                 this.endY = coord[1];
+    //                 this.contextDraft.closePath();
+    //                 this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+    //                 this.contextDraft.beginPath();
+    //                 this.contextDraft.moveTo(this.origX,this.origY);
+    //                 this.contextDraft.quadraticCurveTo(this.origX,this.origY,this.endX,this.endY);
+    //                 this.contextDraft.stroke();
+    //             } else if (this.actionCounter === 1){
+    //                 this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+    //                 this.contextDraft.beginPath();
+    //                 this.contextDraft.moveTo(this.origX,this.origY);
+    //                 this.contextDraft.quadraticCurveTo(coord[0],coord[1],this.endX,this.endY);
+    //                 this.contextDraft.stroke();
+    //             }
+    //         }
+    //         onMouseUp(coord,event){
+    //             if (this.actionCounter === 0){
+    //                 this.actionCounter = 1;
+    //             } else if (this.actionCounter === 1){
+    //                 this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+    //                 this.contextReal.quadraticCurveTo(coord[0],coord[1],this.endX,this.endY);
+    //                 this.contextReal.stroke();
+    //                 this.actionCounter = 0;
+    //                 this.onFinish();
+    //             }
+    //         }
+    //         onFinish(){
+    //             step++;
+    //             console.log(step);
+    //             if(step < pushedArray.length){
+    //               pushedArray.length = step;
+    //             }
+    //             pushedArray.push(canvasReal.toDataURL());
+    //           }
+    // }
